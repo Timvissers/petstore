5581c238976d88b306bb552231100e939eed7971
Tim Vissers <tim.vissers@belgacom.net>
2015-04-29 21:59:43 +0200

    bower update (angular 1.3.15)

--- Gruntfile.js
+++ Gruntfile.js
@@ -310,7 +310,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: 'portfolioApp',
+        module: 'petstoreApp',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
