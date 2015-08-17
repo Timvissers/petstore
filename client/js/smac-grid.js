07eeb5cfcb718149b9fa2e1fef456f23365477d1
Tim Vissers <tim.vissers@belgacom.net>
2015-08-17 17:30:36 +0200

    STORY001 more generic grid directive

--- client/js/smac-grid.js
+++ client/js/smac-grid.js
@@ -1,11 +1,13 @@
 'use strict';
 
-angular.module('petstoreApp').directive('smacGrid', function(){
-    return {
-        restrict: 'E',
-        scope: {
-            list: '='
-        },
-        templateUrl: '../partials/smac-grid.html'
-    };
-});
+angular.module('petstoreApp')
+    .directive('smacGrid', function(){
+        return {
+            restrict: 'E',
+            scope: {
+                list: '=',
+                fields: '='
+            },
+            templateUrl: '../partials/smac-grid.html'
+        };
+    });
